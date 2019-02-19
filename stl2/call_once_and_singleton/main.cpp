2a1d02c1ceb46727b9118a704381a119f4014555
jongwon kang <krnomad@gmail.com>
2019-02-20 01:12:02 +0900

    modify example

--- stl2/call_once_and_singleton/main.cpp
+++ stl2/call_once_and_singleton/main.cpp
@@ -1,44 +1,5 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include "main.hpp"
 #include "_macro.hpp"
-
-#include <cstdio>
-#include <mutex>
-#include <memory>
-
-using namespace std;
-
-class myClass {
-    public:
-        static myClass *getInstance() {
-            call_once(myClass::mOnceFlag, []() {
-                    printf("myClass Instance is created...\n");
-                    mInstance.reset(new myClass);
-                    });
-
-            return mInstance.get();
-        }
-
-        void log() {
-            printf("hello\n");
-        }
-
-        ~myClass() { LOG("%s called", __FUNCTION__); }
-
-
-    private:
-        static unique_ptr<myClass> mInstance;
-        static once_flag mOnceFlag;
-
-        myClass() = default;
-        myClass(const myClass &) = delete;
-        myClass &operator=(const myClass &) = delete;
-};
-
-unique_ptr<myClass> myClass::mInstance;
-once_flag myClass::mOnceFlag;
 
 int main()
 {
